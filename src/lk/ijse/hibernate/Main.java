2d9f49e764ba8a682d56c0f117ec174990762300
lakshanheratg <lakshanherath1101@gmail.com>
2022-06-13 04:18:04 -0700

    testCRUD\

--- src/lk/ijse/hibernate/Main.java
+++ src/lk/ijse/hibernate/Main.java
@@ -10,14 +10,24 @@ public class Main {
     public static void main(String[] args) {
 
         Customer c1 = new Customer(
-                "C002", "lakshan", 28000, "kaluthara"
+                "C002", "lakshan", 28000, "galle"
         );
 
         Session session = FactoryConfiguration.getInstance().getSession();
 
         Transaction transaction = session.beginTransaction();
 
-        session.save(c1);
+        //session.update(c1);
+
+       // Customer customer = session.get(Customer.class, "C001");
+
+        //session.delete(c1);
+
+        Customer customer = session.get(Customer.class, "C002");
+
+        session.delete(customer);
+
+          // session.save(c1);
 
         transaction.commit();
         session.close();
