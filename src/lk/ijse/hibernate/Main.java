faa9b9f6d2896dbf0cf3bb5c36d77fa680f351fa
lakshanheratg <lakshanherath1101@gmail.com>
2022-06-12 21:38:16 -0700

    xml configuration complete and create customer class in entity package

--- src/lk/ijse/hibernate/Main.java
+++ src/lk/ijse/hibernate/Main.java
@@ -1,8 +1,13 @@
 package lk.ijse.hibernate;
 
+import lk.ijse.hibernate.entity.Customer;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        Customer c1 = new Customer("C001", "pathum", 30000, "panadura");
+
 
     }
 }
---
+++ src/lk/ijse/hibernate/entity/Customer.java
@@ -0,0 +1,68 @@
+/**
+ * @author : ALE_IS_TER
+ * Project Name: Hibernate_xml_configur
+ * Date        : 6/12/2022
+ * Time        : 9:34 PM
+ * @Since : 0.1.0
+ */
+
+package lk.ijse.hibernate.entity;
+
+public class Customer {
+    private String id;
+    private String name;
+    private double salary;
+    private String address;
+
+    public Customer() {
+    }
+
+    public Customer(String id, String name, double salary, String address) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+        this.address = address;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", salary=" + salary +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
